a84c4d20cc9abf4ee852f2b8befefc1aa1b8dd2c
orga <orga.shih@gmail.com>
2015-07-25 20:18:59 +0800

    add comment job with function

--- app/jobs/comment_check_job.rb
+++ app/jobs/comment_check_job.rb
@@ -1,8 +1,11 @@
 class CommentCheckJob < ActiveJob::Base
   queue_as :default
 
-  def perform(options={})
+  def perform
     puts "CommentCheckJob: enter: #{Time.now}"
+    EventCommentsCrawler.spawn_json(4).each do |crawler_json|
+      CommentUpdateJob.perform_later(crawler_json)
+    end
     puts "CommentCheckJob: leave: #{Time.now}"
   end
 end
