0a5c33f42e133afa547a09260ab433cfa51f354c
Otis <altispec@gmail.com>
2015-07-25 22:10:13 +0800

    add article template and style

--- app/serializers/article_serializer.rb
+++ app/serializers/article_serializer.rb
@@ -1,6 +1,9 @@
 class ArticleSerializer < ActiveModel::Serializer
-  attributes :id, :event_id, :title, :arthor, :post_at, :start, :content, :article_content, :comments_count, :keywords, :link
+  attributes :article_id, :event_id, :title, :arthor, :post_at, :start, :content, :article_content, :comments_count, :keywords, :link
 
+  def article_id
+    object.id
+  end
   def start
     object.post_at
   end
