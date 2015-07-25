6c1251aea26ee5e4ba025b0cfa5ce66cb67c4f33
Tim-Feng <fsjforever26@gmail.com>
2015-07-26 01:28:25 +0800

    Modify deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,9 @@ set :deploy_to, '/home/deploy/armstrong'
 
 # set :linked_files, %w{config/database.yml}
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp', 'public/system', "public/downloads","publ ic/assets")
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp', 'public/system', "public/downloads","publ ic/assets")
 set :linked_files, %w{config/database.yml config/application.yml config/secrets.yml}
 namespace :deploy do
 
