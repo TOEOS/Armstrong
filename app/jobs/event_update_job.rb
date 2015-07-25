73a4b5d9eec5ddfd5e24a63c31ca126a8992cc8e
orga <orga.shih@gmail.com>
2015-07-26 03:07:00 +0800

    fix event update job wrong args

--- app/jobs/event_update_job.rb
+++ app/jobs/event_update_job.rb
@@ -2,7 +2,7 @@ class EventUpdateJob < ActiveJob::Base
   queue_as :default
   def perform(json)
     puts "EventUpdateJob: enter: #{Time.now}"
-    crawler = EventMonitorCrawler.new(JSON.parse(json))
+    crawler = EventMonitorCrawler.new(unclassed_articles: JSON.parse(json))
     crawler.call
     puts "EventUpdateJob: leave: #{Time.now}"
   end
